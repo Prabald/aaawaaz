afc9c986333e37a44ba00d99d4c8b94b04dfd053
utkarshkala <utkarsh.kala.9@gmail.com>
2019-02-23 13:27:53 +0530

    HuHa

--- about.php
+++ about.php
@@ -126,12 +126,13 @@ else {
       <!-- Faq start -->
       <h1>About Aawaaj</h1> <br>
      
-    <p> In this progressive world we need to align up ourselves with ideas full of innovation and creativity.But what actually happens is while sharing our ideas with the Authorities the communication system,is somewhat weak for letting us to convey those precisely.People often share their problems on social media platforms but between that clumpsy information, the relevance of genuine information gets compromised .What then happens is that neither the authority gets to know about the problems nor we do get to know what their contributions are.This miscommunication often leads to drastic misconceptions between the Authority and the people they serve.This causes rumors and enstrangement among the community.</p>
+    <h6> In this progressive world we need to align up ourselves with ideas full of innovation and creativity.But what actually happens is while sharing our ideas with the Authorities the communication system,is somewhat weak for letting us to convey those precisely.People often share their problems on social media platforms but between that clumpsy information, the relevance of genuine information gets compromised .What then happens is that neither the authority gets to know about the problems nor we do get to know what their contributions are.This miscommunication often leads to drastic misconceptions between the Authority and the people they serve.This causes rumors and enstrangement among the community.
+      <br><br>
 
-    <p>Awaaz is a Social platform for defining the genuine aspects of the society which normally do not get highlighted to the community.Here everyone will be free to raise a concern about a particular problem and it will be made sure that every problem is genuinely sorted, to let the Authorities know the relevant problems.And to let know what problems are correctly solved the people will be free to rate and review their works.What this will provide is that, people will get to know about,how frequently,and with,how much proficiency,an authority plays it's role, in accordance with the words they promise.This will highlight, the best results on the portal, where nothing will be HIDDEN, nothing will be FAKE, and people will let know,the TRUEST ASPECTS of the society.</p>
-<p>
+    Awaaz is a Social platform for defining the genuine aspects of the society which normally do not get highlighted to the community.Here everyone will be free to raise a concern about a particular problem and it will be made sure that every problem is genuinely sorted, to let the Authorities know the relevant problems.And to let know what problems are correctly solved the people will be free to rate and review their works.What this will provide is that, people will get to know about,how frequently,and with,how much proficiency,an authority plays it's role, in accordance with the words they promise.This will highlight, the best results on the portal, where nothing will be HIDDEN, nothing will be FAKE, and people will let know,the TRUEST ASPECTS of the society.
+<br><br>
 Awaaz offers complete transparency,which will help in decreasing Corruption, avoiding unnecessary waste of Economical Finance and truly decoratinge the prestige of The Incredible INDIA!
-</p>
+</h6>
       <!-- Faq end -->
     </div>
     <div class="col-md-2">
